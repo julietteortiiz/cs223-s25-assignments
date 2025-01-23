31a923217aac5689eeef3f3a91c81ef5cac62cae
Juliette Alina Ortiz <jaortiz@goldengate.cs.brynmawr.edu>
2025-01-23 15:01:05 -0500

    test

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,11 +1,12 @@
 /***************************************************
  * mathgame.c
- * Author: 
+ * Author: Juliette Ortiz
  * Implements a math game
  */
 
 #include <stdio.h>
 
 int main() {
+  printf("Test");
   return 0;
 }
