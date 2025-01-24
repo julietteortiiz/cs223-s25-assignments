c7fbd2fed8093ca3706ea5b1a69bed26f45dda2f
Juliette Alina Ortiz <jaortiz@goldengate.cs.brynmawr.edu>
2025-01-24 09:01:39 -0500

    comments

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -6,40 +6,49 @@
  * 2. asking math addition problems by randomly generating numbers
  * 3. checking whether the user's answer was correct or incorrect
  * 4. printing out the number of correct answers
+ * 
+ * @version: January 24, 2024
  */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> //for rand()
 
 
 int main() {
     printf("Welcome to Math Game!\n");
     printf("How many rounds do you want to play?");
     
-    int response = 0;
+    //user input, # of rounds
+    int response = 0; 
     scanf("%d", &response);
     
+    //keeps track of the # of correct answers by the user
     int correctAnswers = 0;
     
     for (int i = 0; i < response; i++){
-        int x = rand();
+        /* x and y are randomly generated #s used in the
+        prompted math question. They're then reduced to 
+        simplify the question. 
+        */
+        int x = rand(); 
         int y = rand();
-        x /= 100000000;
+        x /= 100000000; 
         y /= 100000000;
         printf("%d + %d = ?\n", x, y);
         
+        //stores the user's guess answer to the problem
         int answer = 0;
         scanf("%d", &answer);
         if (answer == (x + y)){
             correctAnswers ++;
             printf("Correct!\n");
-        } else {
+        } 
+        else {
             printf("Incorrect :(\n");
         }
     }
     
     printf("You answered %d/%d correctly.\n", correctAnswers, response);
     
-
     return 0;
 }
